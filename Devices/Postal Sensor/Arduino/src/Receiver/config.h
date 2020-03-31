08cca79745654301de7478083ac083623f8a4be2
Jean Gauthier <gauthier_j100@hotmail.com>
2020-03-30 23:38:09 -0400

    Update config.h

--- Devices/Postal Sensor/Arduino/src/Receiver/config.h
+++ Devices/Postal Sensor/Arduino/src/Receiver/config.h
@@ -13,16 +13,30 @@ const char* mqtt_password       = "yourMQTTcredentials";
 const char* mqtt_deviceName     = "PostalStatus-";
 
 //Control
+const char* mqtt_controlCmd     = "Home/Transmitter/Cmd";
+const char* mqtt_controlStts    = "Home/Transmitter/Stts";
+
 const char* mqtt_postalStts     = "Home/Postal/Stts";
 const char* mqtt_postalCmd      = "Home/Postal/Cmd";
-const char* mqtt_postalSrch     = "Home/Postal/Cmd";
+
+//Sensors
+const char* mqtt_sensorJson     = "Home/Postal/Json";         //MQTT JSON status data
 
 const char* json_postalId       = "id";
 const char* json_postalBat      = "battery";
 const char* json_postalVcc      = "voltage";
 const char* json_postalRssi     = "rssi";
+const char* json_postalSource   = "source";
 
-const char* json_state          = "state";
+const char* json_Sender         = "sender";
+const char* json_Forced         = "forced";
+
+const char* json_delay          = "delay";
+const char* json_test           = "test";
+
+const char* json_reset          = "reset";
+const char* json_rssi           = "rssi";
+const char* json_rssiPercent    = "percent";
 
 //Others
 const char* json_parseFailed    = "parseObject() failed";
@@ -35,7 +49,7 @@ const char* mqtt_cmdOff         = "OFF";
 const char* mqtt_willTopic      = "Home/Postal/LWT";
 const char* mqtt_willOffline    = "offline";
 const char* mqtt_willOnline     = "online";
-const bool  mqtt_willRetain     = true;
+const bool  mqtt_willRetain     = false;
 const byte  mqtt_willQoS        = 0;
 
 //Network settings
