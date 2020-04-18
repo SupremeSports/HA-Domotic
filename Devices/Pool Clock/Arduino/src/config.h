c8c9c048d6df778b246c72017aaa69ba841871e9
Jean Gauthier <gauthier_j100@hotmail.com>
2020-04-17 22:55:19 -0400

    Update config.h

--- Devices/Pool Clock/Arduino/src/config.h
+++ Devices/Pool Clock/Arduino/src/config.h
@@ -10,53 +10,54 @@ const char* mqtt_server         = "192.168.0.xxx";
 const char* mqtt_user           = "yourMQTTcredentials";	
 const char* mqtt_password       = "yourMQTTcredentials";	
 
-const char* mqtt_deviceName     = "PoolClock-";	
+const char* mqtt_deviceName     = "PoolClock-";
 
-const char* mqtt_timeCmd        = "Home/Global/Time";             //Receive time in format YYYY:MM:DD:HH:MM:SS:	
-const char* mqtt_timeRequest    = "Home/Global/Time/Request";     //Request time update	
+const char* mqtt_timeCmd        = "Home/Global/Time";             //Receive time in format YYYY:MM:DD:HH:MM:SS:
+const char* mqtt_timeRequest    = "Home/Global/Time/Request";     //Request time update
 
-//Control	
-const char* mqtt_controlStts    = "Home/Pool/Clock/Stts";         //MQTT JSON CLOCK status data	
-const char* mqtt_controlCmd     = "Home/Pool/Clock/Cmd";          //MQTT JSON CLOCK command data	
+//Control
+const char* mqtt_controlStts    = "Outside/Pool/Clock/Stts";      //MQTT JSON CLOCK status data
+const char* mqtt_controlCmd     = "Outside/Pool/Clock/Cmd";       //MQTT JSON CLOCK command data
 
-const char* json_waterTemp      = "wtemp";	
-const char* json_waterPH        = "wph";	
-const char* json_outTemp        = "otemp";	
-const char* json_outHum         = "ohum";	
-const char* json_timeText       = "uptxt";	
-const char* json_tempText       = "dntxt";	
+const char* json_waterTemp      = "wtemp";
+const char* json_waterPH        = "wph";
+const char* json_outTemp        = "otemp";
+const char* json_outHum         = "ohum";
+const char* json_timeText       = "uptxt";
+const char* json_tempText       = "dntxt";
+const char* json_unavailable    = "unavailable";
 
-//LEDs	
-const char* mqtt_ledStts        = "Home/Pool/Clock/LED/Stts";     //MQTT JSON LIGHT status data	
-const char* mqtt_ledCmd         = "Home/Pool/Clock/LED/Cmd";      //MQTT JSON LIGHT command data	
+//LEDs
+const char* mqtt_ledStts        = "Outside/Pool/Clock/LED/Stts";  //MQTT JSON LIGHT status data
+const char* mqtt_ledCmd         = "Outside/Pool/Clock/LED/Cmd";   //MQTT JSON LIGHT command data
 
-const char* json_state          = "state";	
-const char* json_effect         = "effect";	
-const char* json_brightness     = "brightness";	
-const char* json_color          = "color";	
-const char* json_transition     = "transition";	
+const char* json_state          = "state";
+const char* json_effect         = "effect";
+const char* json_brightness     = "brightness";
+const char* json_color          = "color";
+const char* json_transition     = "transition";
 
-//Sensors	
-const char* mqtt_sensorJson     = "Home/Pool/Clock/Json";         //MQTT JSON status data	
+//Sensors
+const char* mqtt_sensorJson     = "Outside/Pool/Clock/Json";      //MQTT JSON status data
 
-const char* json_reset          = "reset";	
-const char* json_rssi           = "rssi";	
-const char* json_rssiPercent    = "percent";	
+const char* json_reset          = "reset";
+const char* json_rssi           = "rssi";
+const char* json_rssiPercent    = "percent";
 
-//Others	
-const char* json_parseID        = "Parse command ID failed";	
-const char* json_parseFailed    = "parseObject() failed";	
-const char* json_resetReq       = "Reset Requested...";	
-const char* json_resetReboot    = "Rebooting...";	
+//Others
+const char* json_parseID        = "Parse command ID failed";
+const char* json_parseFailed    = "parseObject() failed";
+const char* json_resetReq       = "Reset Requested...";
+const char* json_resetReboot    = "Rebooting...";
 
-const char* mqtt_cmdOn          = "ON";	
-const char* mqtt_cmdOff         = "OFF";	
+const char* mqtt_cmdOn          = "ON";
+const char* mqtt_cmdOff         = "OFF";
 
-const char* mqtt_willTopic      = "Home/Pool/Clock/LWT";	
-const char* mqtt_willOffline    = "offline";	
-const char* mqtt_willOnline     = "online";	
-const bool  mqtt_willRetain     = false;	
-const byte  mqtt_willQoS        = 0;	
+const char* mqtt_willTopic      = "Outside/Pool/Clock/LWT";
+const char* mqtt_willOffline    = "offline";
+const char* mqtt_willOnline     = "online";
+const bool  mqtt_willRetain     = false;
+const byte  mqtt_willQoS        = 0;
 
 //Network settings	
 //MQTT Server IP Address	
