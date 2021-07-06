36df3c59c6cb2da6ee486d17a580efe033d8f08a
Jean Gauthier <gauthier_j100@hotmail.com>
2021-07-06 09:15:22 -0400

    Update WallClockDualStepper.ino

--- Devices/Analog Wall Clock/Arduino/src/WallClockDualStepper.ino
+++ Devices/Analog Wall Clock/Arduino/src/WallClockDualStepper.ino
@@ -27,17 +27,28 @@ CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE G
 /*
   Name:     Dual Stepper Wall Clock
   Created:  2019/04/12
-  Modified: 2020/03/10
   Author:   gauthier_j100@hotmail.com / SupremeSports
-  GitHub:  
+  GitHub:   https://github.com/SupremeSports/HA-Domotic/tree/master/Devices/Analog%20Wall%20Clock
 */
+
+/*
+  ArduinoOTA            1.0.5
+  ESP8266WiFi           
+  PubSubClient          2.8.0
+  ArduinoJson           5.13.5 (6.17.2)
+  
+  Board                 ESP8266-07 - ESP Board (2.7.1) - Generic ESP8266 Module
+*/
+
+const char* version               = "v:9.0.1";
+const char* date                  = "2021/05/10";
 
 // ----------------------------------------------------------------------------------------------------
 // ----------------------------------------- ESP/WIFI DEFINES -----------------------------------------
 // ----------------------------------------------------------------------------------------------------
 #include <ESP8266WiFi.h>
 
-#define ESP8266                 1
+#define ESP8266                     7
 ADC_MODE(ADC_VCC);                                //Read Vcc on ADC input
 
 //Network settings - PLEASE, define those values in a config.h file
@@ -46,10 +57,13 @@ ADC_MODE(ADC_VCC);                                //Read Vcc on ADC input
 IPAddress ip(IP1, IP2, IP3, IP4);                 //Put the current device IP address here
 IPAddress gw(GW1, GW2, GW3, GW4);                 //Put your gateway IP address here
 IPAddress sn(SN1, SN2, SN3, SN4);                 //Put your subnetmask here
+IPAddress dns(GW1, GW2, GW3, GW4);                //Put your DNS here
+IPAddress dns1 = (uint32_t)0x00000000;
+IPAddress dns2 = (uint32_t)0x00000000;
 
-bool networkActive              = false;          //WiFi connectivity status
-int rssiPercent                 = 0;              //WiFi signal strength in percent
-int rssi                        = 0;              //WiFi signal strength in dBm
+bool networkActive                = false;        //WiFi connectivity status
+int rssiPercent                   = 0;            //WiFi signal strength in percent
+int rssi                          = 0;            //WiFi signal strength in dBm
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------- WDT DEFINES --------------------------------------------
@@ -57,16 +71,20 @@ int rssi                        = 0;              //WiFi signal strength in dBm
 #include <Ticker.h>
 Ticker lwdTicker;
 
-#define LWD_TIMEOUT               5000            //WDT Value (ms)
+#define LWD_TIMEOUT                 5000          //WDT Value (ms)
 
-unsigned long lwdTime           = 0;
+unsigned long lwdTime             = 0;
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------- OTA DEFINES --------------------------------------------
 // ----------------------------------------------------------------------------------------------------
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+#define ENABLE_OTA
+
+#ifdef ENABLE_OTA
+  #include <ESP8266mDNS.h>
+  #include <WiFiUdp.h>
+  #include <ArduinoOTA.h>
+#endif
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------ DEBUG DEFINES -------------------------------------------
@@ -81,24 +99,26 @@ unsigned long lwdTime           = 0;
   #define Sprint(a)
 #endif
 
-#ifndef ESP8266
-  #define ESP8266
-#endif
-
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------ OTHER DEFINES -------------------------------------------
 // ----------------------------------------------------------------------------------------------------
 //Used Pins
-const uint8_t boardLedPin       = 2;//LED_BUILTIN;//Pin 1 on ESP-01, pin 2 on ESP-12E/ESP32
-const bool boardLedPinRevert    = true;           //If true, LED is on when output is low
-const bool enableBoardLED       = true;           //If true, LED will flash to indicate status
+const uint8_t boardLedPin         = 2;            //Pin 1 on ESP-01, pin 2 on ESP-12E/ESP32
+const bool boardLedPinRevert      = true;         //If true, LED is on when output is low
+const bool enableBoardLED         = false;        //If true, LED will flash to indicate status
 
 //Variables
-#define initValue               -1                //Initialization value to insure values updates
+#define initValue                   -1            //Initialization value to insure values updates
 
-bool newStart                   = false;          //New start detection
+long lastSecond                   = 0;
 
-long ledFlashDelay              = 0;              //Led flashing delay
+bool newStart                     = false;        //New start detection
+bool ONS_1s                       = false;
+bool ONS_5s                       = false;
+bool ONS_10s                      = false;
+bool ONS_1m                       = false;
+
+bool PRE_5s                       = false;        //Comes on for the second before ONS_5s comes on
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------- MQTT DEFINES -------------------------------------------
@@ -107,84 +127,126 @@ long ledFlashDelay              = 0;              //Led flashing delay
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
-long lastMsg                    = 0;
+
+const uint16_t keepAlive          = 45;
+const uint16_t packetSize         = 1500;
+const uint16_t socketTimeout      = 60;
+
+long lastMsg                      = 0;
 char msg[50];
 
-bool updatePublish              = false;
-bool mqttActive                 = false;
+bool updatePublish                = false;
+bool mqttActive                   = false;
 
-long lastMillis                 = 0;
+bool forceMqttUpdate              = false;
 
 // ----------------------------------------------------------------------------------------------------
 // ---------------------------------------- MQTT JSON DEFINES -----------------------------------------
 // ----------------------------------------------------------------------------------------------------
-#include <ArduinoJson.h>        // https://github.com/bblanchon/ArduinoJson
+#include <ArduinoJson.h>          // https://github.com/bblanchon/ArduinoJson
 
-const int BUFFER_SIZE           = JSON_OBJECT_SIZE(100);
+const int BUFFER_SIZE             = JSON_OBJECT_SIZE(100);//(200);
+const int BUFFER_ARRAY_SIZE       = 255;//512;
+
+char message[BUFFER_ARRAY_SIZE];
 
 // ----------------------------------------------------------------------------------------------------
+// ---------------------------------------- SPECIFIC DEFINES ------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+//const uint8_t sda                 = 0;            //I2C Data pin
+//const uint8_t scl                 = 2;            //I2C Clock pin
+
 // ------------------------------------------- CLOCK DEFINES ------------------------------------------
-// ----------------------------------------------------------------------------------------------------
 //Define installed steppers and comment out the other ones
 #define HOURS
 #define MINS
 //#define SECS
 
 //Used Pins
-#define ResetHoming               0
+#define ResetHoming                 0
 
-#define StepEnable                15
+#define StepEnable                  15
 
-#define HourHall                  16
-#define MinHall                   14
-#define SecHall                   initValue
+#define HourHall                    16
+#define MinHall                     14
+#define SecHall                     initValue
 
-#define HourPulse                 12
-#define MinPulse                  13
-#define SecPulse                  initValue
+#define HourPulse                   12
+#define MinPulse                    13
+#define SecPulse                    initValue
 
-#define Spare1                    initValue//5
-#define Spare2                    initValue//4
+#define Spare1                      initValue     //5
+#define Spare2                      initValue     //4
 
 //Other defines
-#define PULSE_PER_REV             720
-#define STEPPER_HOUR              0               //Motor ID
-#define STEPPER_MIN               1               //Motor ID
-#define STEPPER_SEC               2               //Motor ID
-#define STEPPER_ALL               999
+#define PULSE_PER_REV               720
+#define STEPPER_HOUR                0             //Motor ID
+#define STEPPER_MIN                 1             //Motor ID
+#define STEPPER_SEC                 2             //Motor ID
+#define STEPPER_ALL                 999
 
-#define STEPPER_SPEED             2000            //us between HIGH/LOW
+#define STEPPER_SPEED               2000          //us between HIGH/LOW
 
 //Variables
-int HourTick                    = initValue;
-int MinTick                     = initValue;
-int SecTick                     = initValue;
+int HourTick                      = initValue;
+int MinTick                       = initValue;
+int SecTick                       = initValue;
 
-bool clockHomed                 = false;          // Homing status
+bool clockHomed                   = false;        // Homing status
 
-bool initialHourOns             = false;
-bool initialMinOns              = false;
-bool initialSecOns              = false;
+bool initialHourOns               = false;
+bool initialMinOns                = false;
+bool initialSecOns                = false;
+
+// -------------------------------------- ANALOG SENSORS DEFINES --------------------------------------
+//TODO
+
+// --------------------------------------- SELF VOLTAGE READING ---------------------------------------
+const uint8_t voltage5V_pin       = A0;
+const uint8_t voltage12V_pin      = A0;
+
+float voltage5V                   = 0.0;
+float voltage12V                  = 0.0;
+
+//R1  = 1k
+//R2  = 10k
+//Vr2 = 1.00V @ 5.50V
+//Thorical ratio = 0.90909
+float voltage5VRatio              = 1.0;      //Voltage divider ratio [0 => 5.5]Vdc
+
+//R1  = 10k
+//R2  = 5k6
+//Vr2 = 1.00V @ 13.93V
+//Thorical ratio = 2.78571
+float voltage12VRatio             = 1.0;      //Voltage divider ratio [0 => 13.93]Vdc
+
+// -------------------------------------- DIGITAL SENSORS DEFINES -------------------------------------
+//TODO
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------ TIME CONTROL --------------------------------------------
 // ----------------------------------------------------------------------------------------------------
-#include <TimeLib.h>            //TimeLib library is needed https://github.com/PaulStoffregen/Time
+#include <TimeLib.h>              // TimeLib library is needed https://github.com/PaulStoffregen/Time
+                                  // http://playground.arduino.cc/code/time (Margolis 1.5.0)
 
-#define DELIMITER_TIME            ":"
+#define DELIMITER_TIME              ":"
 
 //Variables
-uint16_t Year                   = initValue;
-uint8_t Month                   = initValue;
-uint8_t Day                     = initValue;
-uint8_t Hour                    = initValue;
-uint8_t Minute                  = initValue;
-uint8_t Second                  = initValue;
-uint8_t DOW                     = initValue;
+uint16_t Year                     = initValue;
+uint8_t Month                     = initValue;
+uint8_t Day                       = initValue;
+uint8_t Hour                      = initValue;
+uint8_t Minute                    = initValue;
+uint8_t Second                    = initValue;
+uint8_t DOW                       = initValue;
 
-uint8_t prevSec                 = initValue;
+uint8_t prevHour                  = initValue;
+uint8_t prevMinute                = initValue;
+uint8_t prevSec                   = initValue;
 
-bool localTimeValid             = false;
+bool localTimeValid               = false;        //Detect that local time is valid
+
+long lastTimeRequestMillis        = millis();
 
 // ----------------------------------------------------------------------------------------------------
 // --------------------------------------------- SETUP ------------------------------------------------
@@ -205,11 +267,13 @@ void setup()
   
   initMQTT();
 
-  lastMillis = millis();
+  lastSecond = millis();
 
   local_delay(50);                                //Wait for all data to be ready
   
   //Initialize data
+  newStart = true;
+  
   clockHomed = false;
   initialHourOns = false;
   initialMinOns = false;
@@ -227,6 +291,8 @@ void loop()
 
   runEverySecond();
 
+  runEveryMinute();
+
   newStart = false;                               //First scan bit
 }
 
@@ -243,12 +309,12 @@ void runEveryScan()
 
   mqttPublish();                                  //Run to check nothing has been received and needs to republish
 
-  local_delay(10);
+  local_delay(5);
 }
 
 void runEverySecond()
 {
-  if (millis()-lastMillis < 1000)
+  if (millis()-lastSecond < 1000)
     return;
 
   unsigned long loopTime = millis();
@@ -256,10 +322,75 @@ void runEverySecond()
   updatePublish = true;
   mqttPublish();                                  //Publish MQTT data
 
-  lastMillis = millis();
+  lastSecond = millis();
 
   loopTime = millis() - loopTime;
   Sprint("Process time: ");
   Sprint(loopTime);
   Sprintln("ms");
+}
+
+void runEvery5seconds()
+{
+  if (!ONS_5s)
+    return;
+
+  ONS_5s = false;
+  PRE_5s = false;
+    
+  Sprintln("5 seconds");
+
+  flashEvery5sec();
+  
+  readSensors(true);
+  local_delay(10);
+  sendDigAnStates();
+  sendSensors();
+}
+
+void runEvery10seconds()
+{
+  if (!ONS_10s)
+    return;
+
+  ONS_10s = false;
+    
+  Sprintln("10 seconds");
+  local_delay(100);
+  
+  //TODO
+}
+
+void runEveryMinute()
+{
+  if (!ONS_1m)
+    return;
+
+  ONS_1m = false;
+
+  unsigned long loopTime = millis();
+
+  //Todo
+
+  loopTime = millis() - loopTime;
+  Sprint("Process time (minute): ");
+  Sprint(loopTime);
+  Sprintln("ms");
+}
+
+void forceUpdateMQTT(bool force)
+{
+  if ((!forceMqttUpdate && !force) || ONS_5s) 
+    return;
+
+  //Do not send data if it is to be sent within the next second
+  if (forceMqttUpdate && PRE_5s)
+  {
+    forceMqttUpdate = false;
+    return;
+  }
+
+  forceMqttUpdate = false;
+
+  //TODO
 }
