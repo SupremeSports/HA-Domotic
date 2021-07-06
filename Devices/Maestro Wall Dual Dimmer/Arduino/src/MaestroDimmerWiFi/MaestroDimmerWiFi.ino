956b637ab2845692911f56f1db1fe813e9b783f2
Jean Gauthier <gauthier_j100@hotmail.com>
2021-07-06 09:28:06 -0400

    Update MaestroDimmerWiFi.ino

--- Devices/Maestro Wall Dual Dimmer/Arduino/src/MaestroDimmerWiFi/MaestroDimmerWiFi.ino
+++ Devices/Maestro Wall Dual Dimmer/Arduino/src/MaestroDimmerWiFi/MaestroDimmerWiFi.ino
@@ -27,13 +27,22 @@ CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE G
 /*
  Name:    Maestro Dimmer Kit - WiFi Canopy
  Created: 2020/11/20
- Created: 2020/12/01
  Author:  gauthier_j100@hotmail.com / SupremeSports
- GitHub:  https://github.com/SupremeSports/
+ GitHub:  https://github.com/SupremeSports/HA-Domotic/blob/master/Devices/Maestro%20Wall%20Dual%20Dimmer
 */
 
-const char* version               = "v:3.0.1";
-const char* date                  = "2020/12/01";
+/*
+  ArduinoOTA            1.0.5
+  UIPEthernet           2.0.9
+  PubSubClient          2.8.0
+  ArduinoJson           6.17.2
+  Wire 
+  DHT sensor library    1.4.1
+  Board                 Arduino Mega 2560 (Optiboot)
+*/
+
+const char* version               = "v:2.3.4";
+const char* date                  = "2021/07/01";
 
 // ----------------------------------------------------------------------------------------------------
 // ----------------------------------------- ESP/WIFI DEFINES -----------------------------------------
@@ -41,6 +50,11 @@ const char* date                  = "2020/12/01";
 #include <WiFi.h>
 
 #define ESP32                       32
+//ADC_MODE(ADC_VCC);                              //Read Vcc on ADC input
+
+#define Switch1     //SN: 00001
+//#define Switch2     //SN: 00002
+//#define Switch3     //SN: 00003
 
 //Network settings - PLEASE, define those values in a config.h file
 #include "config.h"
@@ -48,13 +62,18 @@ const char* date                  = "2020/12/01";
 IPAddress ip(IP1, IP2, IP3, IP4);                 //Put the current device IP address here
 IPAddress gw(GW1, GW2, GW3, GW4);                 //Put your gateway IP address here
 IPAddress sn(SN1, SN2, SN3, SN4);                 //Put your subnetmask here
+IPAddress dns(GW1, GW2, GW3, GW4);                //Put your DNS here
+IPAddress dns1 = (uint32_t)0x00000000;
+IPAddress dns2 = (uint32_t)0x00000000;
 
 bool networkActive                = false;        //WiFi connectivity status
 int rssiPercent                   = 0;            //WiFi signal strength in percent
 int rssi                          = 0;            //WiFi signal strength in dBm
 
-bool testStandalone               = true;
+bool standaloneMode               = false;
 uint8_t pinStandalone             = 36;
+
+bool testStandalone               = false;
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------- WDT DEFINES --------------------------------------------
@@ -62,21 +81,23 @@ uint8_t pinStandalone             = 36;
 hw_timer_t *watchdogTimer         = NULL;         //Watchdog implementation
 #define LWD_TIMEOUT                 5000          //WDT Value (ms)
 
-#include <rom/rtc.h>
-
-const uint8_t selfResetPin        = 25;           //Set to output ONLY when ready to reset
+//#include <rom/rtc.h>
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------- OTA DEFINES --------------------------------------------
 // ----------------------------------------------------------------------------------------------------
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+#define ENABLE_OTA
+
+#ifdef ENABLE_OTA
+  #include <ESPmDNS.h>
+  #include <WiFiUdp.h>
+  #include <ArduinoOTA.h>
+#endif
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------ DEBUG DEFINES -------------------------------------------
 // ----------------------------------------------------------------------------------------------------
-#define DEBUG
+//#define DEBUG
 
 #ifdef DEBUG
   #define Sprintln(a) (Serial.println(a))
@@ -85,6 +106,8 @@ const uint8_t selfResetPin        = 25;           //Set to output ONLY when read
   #define Sprintln(a)
   #define Sprint(a)
 #endif
+
+#define BAUDRATE                    115200
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------ OTHER DEFINES -------------------------------------------
@@ -95,11 +118,17 @@ const bool boardLedPinRevert      = false;        //If true, LED is on when outp
 bool enableBoardLED               = false;        //If true, LED will flash to indicate status
 
 //Variables
-#define initValue                 -1              //Initialization value to insure values updates
+#define initValue                   -1            //Initialization value to insure values updates
+
+long lastSecond                   = 0;
 
 bool newStart                     = false;        //New start detection
+bool ONS_1s                       = false;
+bool ONS_5s                       = false;
+bool ONS_10s                      = false;
+bool ONS_1m                       = false;
 
-long ledFlashDelay                = 0;            //Led flashing delay
+bool PRE_5s                       = false;        //Comes on for the second before ONS_5s comes on
 
 // ----------------------------------------------------------------------------------------------------
 // ------------------------------------------- MQTT DEFINES -------------------------------------------
@@ -108,14 +137,18 @@ long ledFlashDelay                = 0;            //Led flashing delay
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
+
+const uint16_t keepAlive          = 45;
+const uint16_t packetSize         = 256;//1500;
+const uint16_t socketTimeout      = 60;
+
 long lastMsg                      = 0;
 char msg[50];
 
 bool updatePublish                = false;
 bool mqttActive                   = false;
 
-long lastSecond                   = 0;
-long lastMinute                   = 0;
+bool forceMqttUpdate              = false;
 
 // ----------------------------------------------------------------------------------------------------
 // ---------------------------------------- MQTT JSON DEFINES -----------------------------------------
@@ -128,60 +161,67 @@ const int BUFFER_ARRAY_SIZE       = 255;
 char message[BUFFER_ARRAY_SIZE];
 
 // ----------------------------------------------------------------------------------------------------
-// ------------------------------------------ NRF24 DEFINES -------------------------------------------
+// ---------------------------------------- SPECIFIC DEFINES ------------------------------------------
 // ----------------------------------------------------------------------------------------------------
-#include <SPI.h>
-#include <RH_NRF24.h>
+const uint8_t sda                 = 21;          //I2C Data pin
+const uint8_t scl                 = 22;          //I2C Clock pin
 
-// Singleton instance of the radio driver
-//RH_NRF24 nrf24(12, 5);
-RH_NRF24 nrf24(16, 17); //(CE, CSN)
-
-uint8_t nrfChannel                = 2;
-uint16_t switchID                 = 0x0003;
-uint16_t switchRcvdID             = 0;
-
-long lastDataNRF                  = 0;                            //Last communication time
-bool sendDataNRF                  = false;
-
-uint8_t PWM_OFFSET                = 0;                            //% Light level percentage
-
-bool permissionSendNRF            = false;                        //Prevent operation before reading EEPROM from switch
-
-// ----------------------------------------------------------------------------------------------------
 // ------------------------------------------- I2C DEFINES --------------------------------------------
-// ----------------------------------------------------------------------------------------------------
 #include <Arduino.h>
 #include <Wire.h>
-//#include <WireSlave.h>
 
-#define SDA_PIN                     21
-#define SCL_PIN                     22
-#define I2C_SLAVE_ADDR              0x04
+const uint8_t addressI2C          = 0x04;
+const uint8_t bytesI2C            = 32;
 
-// set the max number of bytes the slave will send.
-// if the slave send more bytes, they will still be read
-// but the WireSlaveRequest will perform more reads
-// until a whole packet is read
-#define MAX_SLAVE_RESPONSE_LENGTH   32
+const uint8_t hrtByte             = 0;
+const uint8_t idByte              = 1;
 
-#define I2C_IRQ                     5
-
-uint8_t heartbeatIn               = 0;
-uint8_t heartbeatOut              = 0;
+const uint8_t irqI2C              = 5;
 
 long lastDataI2C                  = 0;                            //Last communication time
 bool sendDataI2C                  = false;
 
-uint8_t optionData                = 0;
 bool enableRemoteLED              = false;
-#define LED_BIT                     0                             //Enable/Disable onboard LED 1=ON
 
 bool permissionSendI2C            = false;                        //Prevent operation before reading EEPROM from switch
 
-// ----------------------------------------------------------------------------------------------------
+// ------------------------------------------ NRF24 DEFINES -------------------------------------------
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define RH_NRF24_MAX_MESSAGE_LEN    28
+
+RF24 radio(16, 17); // CE, CSN
+
+#if defined(Switch1)
+const byte pipes[][6] = {"00001","00002"};                        //Pipes should be unique and switchID should be equal to [0]
+const uint16_t switchID           = 0x01;                         //Pipes should be unique and switchID should be equal to [0]
+#elif defined(Switch2)
+const byte pipes[][6] = {"00003","00004"};                        //Pipes should be unique and switchID should be equal to [0]
+const uint16_t switchID           = 0x03;                         //Pipes should be unique and switchID should be equal to [0]
+#elif defined(Switch3)
+const byte pipes[][6] = {"00005","00006"};                        //Pipes should be unique and switchID should be equal to [0]
+const uint16_t switchID           = 0x05;                         //Pipes should be unique and switchID should be equal to [0]
+#else
+#error You MUST define a SwitchID
+#endif
+
+const char* delim                 = ":";
+
+long lastDataNRF                  = 0;
+uint8_t heartbeatIn               = 0;
+uint8_t heartbeatOut              = 0;
+
+bool sendDataNRF                  = false;
+bool permissionSendNRF            = false;                        //Prevent operation before reading EEPROM from switch
+
+uint8_t optionData                = 0;
+#define LED_BIT                     0                             //Enable/Disable onboard LED 1=ON
+
+uint8_t PWM_OFFSET                = 0;                            //% Light level percentage
+
 // --------------------------------------- LIGHT & FAN DEFINES ----------------------------------------
-// ----------------------------------------------------------------------------------------------------
 typedef struct
 {
   uint8_t level;
@@ -218,7 +258,12 @@ bool updateMQTT                   = false;
 bool newDataNRF                   = false;
 bool updateNRF                    = false;
 
-bool standaloneMode               = false;
+// ---------------------------------------- DHT SENSOR DEFINES ----------------------------------------
+float DHTTempOut                  = initValue;    //DHT temperature output to MQTT
+float DHTHumOut                   = initValue;    //DHT humidity output to MQTT
+
+float DHTTempIn                   = initValue;    //DHT temperature output to MQTT
+float DHTHumIn                    = initValue;    //DHT humidity output to MQTT
 
 // --------------------------------------- SELF VOLTAGE READING ---------------------------------------
 const uint8_t voltage5V_pin       = A5;
@@ -232,13 +277,26 @@ float voltage5VSwitch             = 0.0;
 //Thorical ratio = 0.6
 float voltage5VRatio              = 1.10000;      //Voltage divider ratio [0 => 5.5]Vdc
 
+// ---------------------------------------- SELF-RESET DEFINES ----------------------------------------
+const uint8_t selfResetPin        = 25;           //Set to output ONLY when ready to reset
+
+// ----------------------------------------------------------------------------------------------------
+// ------------------------------------------ TIME CONTROL --------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+#include <TimeLib.h>              // TimeLib library is needed https://github.com/PaulStoffregen/Time
+                                  // http://playground.arduino.cc/code/time (Margolis 1.5.0)
+
+//Variables
+uint8_t Second                    = initValue;
+uint8_t Minute                    = initValue;
+
 // ----------------------------------------------------------------------------------------------------
 // ---------------------------------------------- SETUP -----------------------------------------------
 // ----------------------------------------------------------------------------------------------------
 void setup()
 {
   #ifdef DEBUG
-    Serial.begin(115200);
+    Serial.begin(BAUDRATE);
     while (!Serial);
   #endif
 
@@ -275,6 +333,7 @@ void loop()
   runEveryScan();
 
   runEverySecond();
+  
   runEveryMinute();
 
   newStart = false;                               //First scan bit
@@ -307,9 +366,14 @@ void runEverySecond()
     lastSecond = millis();
   
     loopTime = millis() - loopTime;
-    /*Sprint("Process time: ");
+    Sprint("Process time: ");
     Sprint(loopTime);
-    Sprintln("ms");*/
+    Sprintln("ms");
+
+    //Sprintln();
+    //Sprintln(heartbeatIn);
+    //Sprintln(heartbeatOut);
+    //Sprintln();
   #else
     updatePublish = true;
     mqttPublish();                                  //Publish MQTT data
@@ -318,18 +382,65 @@ void runEverySecond()
   #endif
 }
 
+void runEvery5seconds()
+{
+  if (!ONS_5s)
+    return;
+  
+  ONS_5s = false;
+  PRE_5s = false;
+
+  #ifdef DEBUG
+    Sprintln("5 seconds");
+  #endif
+
+  flashEvery5sec();
+
+  readSensors(true);
+  local_delay(10);
+  sendSensors();
+  local_delay(5);
+  updatePublish = true;
+}
+
+void runEvery10seconds()
+{
+  if (!ONS_10s)
+    return;
+    
+  ONS_10s = false;
+
+  //After 10 seconds without communication with wall switch, force canopy to work without it through HA
+  if (!permissionSendI2C)
+  {
+    permissionSendI2C = true;
+    lampNRF.state = false;
+    lampNRF.fade = true;
+
+    fanNRF.state = false;
+    fanNRF.fade = false;
+
+    newDataNRF = true;
+  }
+
+  #ifdef DEBUG
+    Sprintln("10 seconds");
+  #endif
+  //TODO
+}
+
 void runEveryMinute()
 {
-  if (millis()-lastMinute < 60000)
+  if (!ONS_1m)
     return;
+
+  ONS_1m = false;
 
   #ifdef DEBUG
     unsigned long loopTime = millis();
 
     sendLightState();
     sendFanState();
-  
-    lastMinute = millis();
   
     loopTime = millis() - loopTime;
     Sprint("Process time (minute): ");
@@ -338,7 +449,22 @@ void runEveryMinute()
   #else
     sendLightState();
     sendFanState();
-    
-    lastMinute = millis();
   #endif
+}
+
+void forceUpdateMQTT(bool force)
+{
+  if ((!forceMqttUpdate && !force) || ONS_5s) 
+    return;
+
+  //Do not send data if it is to be sent within the next second
+  if (forceMqttUpdate && PRE_5s)
+  {
+    forceMqttUpdate = false;
+    return;
+  }
+
+  forceMqttUpdate = false;
+
+  //TODO
 }
