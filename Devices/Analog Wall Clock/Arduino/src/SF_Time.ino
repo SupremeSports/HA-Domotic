bb8f91d75000503cbf26e18315dd9106eac74fad
Jean Gauthier <gauthier_j100@hotmail.com>
2021-07-06 09:15:09 -0400

    Update SF_Time.ino

--- Devices/Analog Wall Clock/Arduino/src/SF_Time.ino
+++ Devices/Analog Wall Clock/Arduino/src/SF_Time.ino
@@ -2,24 +2,29 @@
 // ----------------------------------------- TIME FUNCTIONS -------------------------------------------
 // ----------------------------------------------------------------------------------------------------
 //IMPORTANT - Always send a delimiter at the of string for parsing to succeed
-void parseTime(String data)
+//Expected payload: "YYYY:MM:DD:hh:mm:ss:"
+void parseTime(char* message)
 {
   int dataSize = 6;
-  int indexDelim[dataSize]; // ALWAYS END WITH A DELIMITER
+  uint16_t dateTime[dataSize]; // ALWAYS END WITH A DELIMITER
+  char data[5];
 
-  indexDelim[0] = data.indexOf(DELIMITER_TIME);
-  for (int i = 1; i<dataSize-1; i++)
-    indexDelim[i] = data.indexOf(DELIMITER_TIME, indexDelim[i-1]+1);
-  indexDelim[dataSize-1] = data.lastIndexOf(DELIMITER_TIME);
-
-  uint16_t dateTime[dataSize];
-  dateTime[0] = data.substring(0, indexDelim[0]).toInt();
-  for (int i = 1; i<dataSize; i++)
-    dateTime[i] = data.substring(indexDelim[i-1]+1, indexDelim[i]).toInt();
+  dateTime[0] = atoi(strncpy(data, message, 4));
+  int k = 1;
+  for (int i=4; i<18; i++)    //dateTime array
+  {
+    for (int j=0; j<2; j++)   //character array
+    {
+      i++; //Skip delimiter and/or go to next character
+      data[j] = message[i];
+    }
+    data[2] = '\0';
+    dateTime[k] = atoi(data);
+    k++; //Next parsing
+  }
 
   setTime(dateTime[3], dateTime[4], dateTime[5], dateTime[2], dateTime[1], dateTime[0]);
 
-  Second = 100;
   updateTime();
 
   localTimeValid = (Year>2000);
@@ -27,8 +32,15 @@ void parseTime(String data)
 
 void updateTime()
 {
+  ONS_1s = false;
+  ONS_5s = false;
+  ONS_10s = false;
+  ONS_1m = false;
   if (second() == Second)
     return;
+
+  if (minute() != Minute)
+    ONS_1m = true;
   
   Year = year();
   Month = month();
@@ -39,4 +51,10 @@ void updateTime()
   DOW = weekday()-1;
 
   localTimeValid = (Year>2000);
+
+  ONS_1s = true;
+  ONS_5s = Second%5==0;
+  ONS_10s = Second%10==0;
+
+  PRE_5s = (Second+1)%5==0 || ONS_5s; //Less than a second before next trigger
 }
