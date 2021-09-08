e813be4274d7df26e7a6f2719fcbabd07c1750ff
SupremeSports <gauthier_j100@hotmail.com>
2021-09-08 12:47:25 -0400

    Update for templates sensor/binary_sensor
    Updated to get the new template format under "template: !include_dir_merge_list Templates"

--- HomeAssistant/config/custom_components/hello_state.py
+++
@@ -1,6 +0,0 @@
-DOMAIN = 'hello_state'
-
-def setup(hass, config):
-    hass.states.set('hello.world', 'Works!')
-
-    return True
--- HomeAssistant/config/custom_components/mqtt_template/__init__.py
+++
@@ -1,5 +0,0 @@
-"""mqtt template switch"""
-
-__version__ = "1.0"
-
-DOMAIN = "mqtt_template"
